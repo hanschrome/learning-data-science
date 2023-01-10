852b0f0901508e6dde29fb99af64b48038382cb2
Hans Castro <hanscastroadmn@gmail.com>
2023-01-10 21:49:18 +0100

    Simple linear regression

--- laboratory/quiniela-dataset/3_preprocessing.py
+++ laboratory/quiniela-dataset/3_preprocessing.py
@@ -1,25 +1,21 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 # Read the CSV file into a Pandas DataFrame
 df = pd.read_csv("datasets/output.csv", names=["local", "visitante", "signo", "fecha_completa"])
 
-df["fecha_completa"] = pd.to_datetime(df["fecha_completa"], unit='s', errors='coerce')
-
-df["year"] = df["fecha_completa"].dt.year
-df["month"] = df["fecha_completa"].dt.month
-df["day"] = df["fecha_completa"].dt.day
-
-# Drop the original fecha_completa column
-df = df.dropna(subset=["fecha_completa"])
-
 # Convert the signo column to a categorical data type
 df["signo"] = df["signo"].astype("category")
+
+# Use LabelEncoder to convert the signo column to numerical values
+label_encoder = LabelEncoder()
+df["signo"] = label_encoder.fit_transform(df["signo"])
 
 # Encode the categorical variables as numerical data
 df = pd.get_dummies(df, columns=["local", "visitante"])
 
 # Split the data into features (X) and labels (y)
-X = df.drop("signo", axis=1)
+X = df.drop(["signo"], axis=1)
 y = df["signo"]
 
 # Split the data into training and testing sets
@@ -38,3 +34,18 @@ print("\nSigno value counts in y_train:")
 print(y_train.value_counts())
 print("\nSigno value counts in y_test:")
 print(y_test.value_counts())
+
+print(X_train)
+
+"""
+    Simple linear regression model
+"""
+
+from sklearn import linear_model
+
+linear_regression_model = linear_model.LinearRegression()
+linear_regression_model.fit(X_train, y_train)
+
+predicts = linear_regression_model.predict(X_test)
+
+print(predicts)
